88fd6634240ed03d982df5a790969481156b600c
dhong9 <danielhong35@gmail.com>
2025-03-27 16:15:18 -0700

    Rewrote into a class

--- test.js
+++ test.js
@@ -15,5 +15,16 @@ describe("Game Initialization", () => {
             [0, 0, 0, 0, 0, 0, 0, 0]
         ]
         assert.deepEqual(othello.board, expectedBoard);
-    })
+    });
+});
+
+describe("Game Utilities", () => {
+    it("Verifies that tokens are in bounds", () => {
+        const othello = new Othello_Danyo();
+        assert.ok(othello.inBounds(2, 5));
+        assert.ok(othello.inBounds(0, 0));
+        assert.ok(othello.inBounds(7, 7));
+        assert.ok(!othello.inBounds(-1, -2));
+        assert.ok(!othello.inBounds(9, 8));
+    });
 })
