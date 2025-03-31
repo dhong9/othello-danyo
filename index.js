3baac418c53585d3425fb57d4496b8582c13119b
dhong9 <danielhong35@gmail.com>
2025-03-30 21:06:58 -0700

    Added documentation to functions and class

--- index.js
+++ index.js
@@ -1,8 +1,19 @@
+/**
+ * Othello game class with min/max and reinforcement learning algorithms
+ */
 class Othello_Danyo {
+
+    /**
+     * Constructor for the Othello class
+     */
     constructor() {
         this.board = this.createBoard();
     }
 
+    /**
+     * Defines game board starting state
+     * @returns Default game board starting state
+     */
     createBoard() {
         // Default all values to 0
         const board = [];
@@ -25,10 +36,28 @@ class Othello_Danyo {
         return board;
     }
 
+    /**
+     * Checks if requested row and column are within the game board
+     * @param {Number} r 
+     * @param {Number} c 
+     * @returns true if row and column are in bounds
+     */
     inBounds(r, c) {
         return r >= 0 && r < 8 && c >= 0 && c < 8;
     }
 
+    /**
+     * Flips pieces to the player's color until it reaches the board's
+     * edge or an opponent's piece
+     * @param {Array<Array<Number>>} board the game board
+     * @param {Number} r row
+     * @param {Number} c column
+     * @param {Number} deltaRow row direction 
+     * @param {Number} deltaCol column direction
+     * @param {Number} myPiece player's piece
+     * @param {Number} opponentPiece opponent's piece
+     * @returns resulting board after pieces are flipped
+     */
     flipPieces(board, r, c, deltaRow, deltaCol, myPiece, opponentPiece) {
         while (this.inBounds(r, c) && board[r][c] === opponentPiece) {
             board[r][c] = myPiece;
@@ -38,6 +67,17 @@ class Othello_Danyo {
         return board;
     }
 
+    /**
+     * Checks if any pieces can be flipped in a given direction
+     * @param {Array<Array<Number>>} board the game board
+     * @param {Number} r row
+     * @param {Number} c column
+     * @param {Number} deltaRow row direction
+     * @param {Number} deltaCol column piece
+     * @param {Number} myPiece player's piece
+     * @param {Number} opponentPiece 
+     * @returns true if any pieces can be flipped
+     */
     checkFlip(board, r, c, deltaRow, deltaCol, myPiece, opponentPiece) {
         if (this.inBounds(r, c) && board[r][c] === opponentPiece) {
             while (this.inBounds(r + deltaRow, c + deltaCol)) {
@@ -51,6 +91,14 @@ class Othello_Danyo {
           return false; // Either no consecutive opponent pieces or hit the edge
     }
 
+    /**
+     * Checks if a piece can be placed in given location
+     * @param {Array<Array<Number>>} board the game board
+     * @param {Number} r row
+     * @param {Number} c column
+     * @param {Number} piece player's piece
+     * @returns true if move is valid
+     */
     validMove(board, r, c, piece) {
         // Check that the coordinates are empty
         if (!this.inBounds(r, c) || board[r][c]) return false;
@@ -72,6 +120,12 @@ class Othello_Danyo {
         return directions.some(([deltaRow, deltaCol]) => this.checkFlip(board, r + deltaRow, c + deltaCol, deltaRow, deltaCol, piece, opponent));
     }
 
+    /**
+     * Gets coordinates that a piece can be played
+     * @param {Array<Array<Number>>} board the game board
+     * @param {*} piece player's piece
+     * @returns Array of legal coordinates
+     */
     getValidMoves(board, piece) {
         const moves = [];
 
@@ -84,12 +138,24 @@ class Othello_Danyo {
         return moves;
     }
 
+    /**
+     * Checks if game is over
+     * @param {Array<Array<Number>>} board the game board
+     * @returns true if game is over
+     */
     gameOver(board) {
         const moves1 = this.getValidMoves(board, 1),
               moves2 = this.getValidMoves(board, 2);
         return moves1.length < 1 || moves2.length < 1;
     }
 
+    /**
+     * Plays a piece on the board
+     * @param {*} board the game board
+     * @param {*} r row
+     * @param {*} c column
+     * @param {*} piece player's piece
+     */
     makeMove(board, r, c, piece) {
         // Put the piece at x, y
         board[r][c] = piece;
@@ -116,10 +182,31 @@ class Othello_Danyo {
         }
     }
 
+    /**
+     * Provides string representation of the current game state in FEN notation
+     * @param {Array<Array<Number>>} board the game board
+     * @param {Number} curPlayer current player
+     * @returns FEN representation of the board
+     */
     fen(board, curPlayer) {
-        return board.map(row => row.map(curr => " bw"[curr]).join('').replace(/ +/g, x => x.length)).join('/') + ' ' + (curPlayer === 2 ? 'w' : 'b');
+        return board.map(
+            // 0 -> ' ' (space)
+            // 1 -> 'b' (black)
+            // 2 -> 'w' (white)
+            row => row.map(curr => " bw"[curr])
+        .join('')
+        // Compress consecutive blank spaces into a number representing how many
+        .replace(/ +/g, x => x.length))
+        // Separate each row with a '/'
+        .join('/') + ' ' + (curPlayer === 2 ? 'w' : 'b'); // Append active player to the end
     }
 
+    /**
+     * Tallies how many of a player's piece is on the board
+     * @param {Array<Array<Number>>} board the game board
+     * @param {number} piece player's piece
+     * @returns player's current score
+     */
     score(board, piece) {
         let total = 0;
         for (const row of board)
@@ -128,16 +215,35 @@ class Othello_Danyo {
         return total;
     }
 
+    /**
+     * Provides a deep copy of the board
+     * @param {Array<Array<Number>>} board the game board
+     * @returns Copy of the board
+     */
     copyBoard(board) {
         return board.map(row => [...row]);
     }
 
+    /**
+     * Computes net gain/loss in relation to the current player
+     * @param {Array<Array<Number>>} board the game board
+     * @param {Number} whoseTurn current player
+     * @returns Score difference between current player and opponent
+     */
     heuristic(board, whoseTurn) {
         const opponent = whoseTurn === 2 ? 1 : 2;
         const ourScore = this.score(board, whoseTurn), oppScore = this.score(board, opponent);
         return ourScore - oppScore;
     }
 
+    /**
+     * Calculates gain/loss given the board's current state
+     * @param {Array<Array<Number>>} board the game board
+     * @param {*} originalTurn original player to base score on
+     * @param {*} currentTurn current player
+     * @param {*} searchPly current search depth
+     * @returns min/max score
+     */
     minimaxValue(board, originalTurn, currentTurn, searchPly) {
         if (searchPly === 5 || this.gameOver(board)) // Change to desired ply lookahead
             return this.heuristic(board, originalTurn);
@@ -168,6 +274,12 @@ class Othello_Danyo {
         return bestMoveVal;
     }
 
+    /**
+     * Uses min/max algorithm to compute the best move
+     * @param {*} board the game board
+     * @param {*} whoseTurn current player
+     * @returns Best computed move
+     */
     minimaxDecision(board, whoseTurn) {
         const opponent = whoseTurn === 2 ? 1 : 2;
 
@@ -196,6 +308,16 @@ class Othello_Danyo {
         return [bestX, bestY]
     }
 
+    /**
+     * Updates given Q-table
+     * @param {Object} qTable Q table
+     * @param {String} state board state in string format
+     * @param {Number} action number that combines row with column
+     * @param {String} newState board updated state in string format
+     * @param {Number} reward how good/bad the move was
+     * @param {Number} learningRate learning rate
+     * @param {Number} discountFactor discount factor
+     */
     updateQValues(qTable, state, action, newState, reward, learningRate=0.1, discountFactor=0.9) {
         if (!(state in qTable))
             qTable[state] = {};
@@ -208,6 +330,12 @@ class Othello_Danyo {
         qTable[state][action] += learningRate * (reward + discountFactor * maxFutureQ - qTable[state][action]);
     }
 
+    /**
+     * Runs games of Othello with reinforcement learning
+     * @param {Array<Array<Number>>} board  the game board
+     * @param {Number} rounds Number of games to train in one sitting
+     * @returns Q tables for both players
+     */
     train(board, rounds) {
         const qTable_white = {}, qTable_black = {};
         let curPlayer = 1;
